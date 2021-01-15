9ef709f892b69189e53c956d85d434299362b3c4
v_hujing01_dxm <v_hujing01@duxiaoman.com>
2021-01-15 23:11:45 +0800

    新增元类

--- class05.py
+++ class05.py
@@ -88,8 +88,77 @@ m = Model()
 # m.name = 'bais'
 # print(m.name)
 m.attr = 1000
+
+
 # del m.attr
-print(m.attr)
+# print(m.attr)
 
 
+class CharFiled:
+    def __init__(self, max_lenght=20):
+        self.max_lenght = max_lenght
 
+    def __get__(self, instance, owner):
+        return self.value
+
+    def __set__(self, instance, value):
+        if isinstance(value, str):
+            if len(value) <= self.max_lenght:
+                self.value = value
+            else:
+                raise ValueError('字符串长度应该在{}以内'.format(self.max_lenght))
+        else:
+            raise TypeError("need a str")
+
+    def __delete__(self, instance):
+        self.value = None
+
+
+class IntFiled:
+
+    def __get__(self, instance, owner):
+        return self.value
+
+    def __set__(self, instance, value):
+        if isinstance(value, int):
+            self.value = value
+        else:
+            raise TypeError("need a int")
+
+    def __delete__(self, instance):
+        self.value = None
+
+
+class UserModel(object):
+    # 假设这个是模型类
+    name = CharFiled(max_lenght=30)
+    pwd = CharFiled(max_lenght=40)
+    age = IntFiled()
+
+
+m = UserModel()
+m.name = 'baisbjjjjjjjjjjjjjjjj'
+m.pwd = 'iufddddddddiuwehfiuwehuiwehew'
+m.age = 18
+print(m.name)
+print(m.pwd)
+print(m.age)
+
+
+# 经典类  继承 instance类型（Python2）
+class MyClass:
+    pass
+
+
+# 新式类  继承object（Python3）
+class Test(object):
+    pass
+
+
+t = Test()
+print(type(t))
+print(type(Test))
+print(type(type))
+
+# type python3中所以的类都是通过type创建出来的：元类
+# object  Python3中所以类的顶级父类都是object
